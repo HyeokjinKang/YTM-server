e65a567a9d9d2f5702cab6b873e5c047d4913101
HyeokjinKang <coupy@croissant.tech>
2022-03-10 13:56:39 +0900

    add: error 응답을 반환하는 함수 추가

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, Response
 from ytmusicapi import YTMusic
 
 app = Flask(__name__)
 ytmusic = YTMusic('headers_auth.json')
+
+def errorResponse(error, code):
+  return Response('{"Error": %s}' % (error), status=code, mimetype='application/json')
 
 @app.route('/', methods=['GET'])
 def index():
